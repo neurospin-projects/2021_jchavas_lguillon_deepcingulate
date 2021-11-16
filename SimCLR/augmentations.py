Solves rotation bug

SimCLR/augmentations.py
@@ -164,18 +164,22 @@ class RotateTensor(object):
         onehot_im = im_encoder.fit_transform(flat_im)
         ## rotate one hot im
         onehot_im = onehot_im.reshape(*arr_shape, -1)
-        onehot_im_rot = np.empty_like(onehot_im)
+        onehot_im_result = np.copy(onehot_im)
         n_cat = onehot_im.shape[-1]
         for axes in (0,1), (0,2), (1,2):
             np.random.seed()
             angle = np.random.uniform(-self.max_angle, self.max_angle)
-            onehot_im_rot = rotate(onehot_im,
-                                angle=angle,
-                                axes=axes,
-                                reshape=False,
-                                mode='constant',
-                                cval=0)
-        im_rot_flat = im_encoder.inverse_transform(np.reshape(onehot_im_rot, (-1, n_cat)))
+            onehot_im_rot = np.empty_like(onehot_im)
+            for c in range(n_cat):
+                const = 1 if c == 0 else 0
+                onehot_im_rot[...,c] = rotate(onehot_im_result[...,c],
+                                                angle=angle,
+                                                axes=axes,
+                                                reshape=False,
+                                                mode='constant',
+                                                cval=const)
+            onehot_im_result = onehot_im_rot
+        im_rot_flat = im_encoder.inverse_transform(np.reshape(onehot_im_result, (-1, n_cat)))
         im_rot = np.reshape(im_rot_flat, arr_shape)
         arr_rot = np.expand_dims(
             im_rot,