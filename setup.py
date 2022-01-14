6f7a3c62fd5f52770a3ae79842093cfeb3d13406
JoelChavas <joel.chavas@gmail.com>
2022-01-14 16:49:39 +0100

    Makes it install through a single pip3 install command

--- setup.py
+++ setup.py
@@ -1,20 +1,34 @@
+import os
 from setuptools import setup, find_packages
 
 setup(
     name='2021_jchavas_lguillon_deepcingulate',
     version='0.0.1',
-    packages=find_packages(exclude=['tests*', 'notebooks*']),
+    packages=find_packages(
+        exclude=['tests*', 'notebooks*']),
     license='CeCILL license version 2',
-    description='Deep learning models to analyze anterior cingulate sulcus patterns',
+    description='Deep learning models '
+                'to analyze anterior cingulate sulcus patterns',
     long_description=open('README.rst').read(),
-    install_requires=['deep_folding', 'pandas', 
-                    'scipy', 'matplotlib',
-                    'torch', 'tqdm',
-                    'torchvision', 'torch-summary', 'hydra', 'hydra.core',
-                    'dataclasses', 'hydra', 'OmegaConf',
-                    'sklearn', 'scikit-image',
-                    'pytorch-lightning', 'lightly',
-                    'toolz', ],
+    install_requires=['pandas',
+                      'scipy',
+                      'matplotlib',
+                      'torch',
+                      'tqdm',
+                      'torchvision',
+                      'torch-summary',
+                      'hydra',
+                      'hydra.core',
+                      'dataclasses',
+                      'OmegaConf',
+                      'sklearn',
+                      'scikit-image',
+                      'pytorch-lightning',
+                      'lightly',
+                      'toolz',
+                      'deep_folding @ \
+                        git+https://git@github.com/neurospin/deep_folding',
+            ],
     url='https://github.com/neurospin-projects/2021_jchavas_lguillon_deepcingulate',
     author='Joël Chavas, Louise Guillon',
     author_email='joel.chavas@cea.fr, louise.guillon@cea.fr'
